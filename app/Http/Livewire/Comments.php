3ea9eb3c4aa01a2a290d75224590a1b0511a2ef3
Michael9621 <oandami@gmail.com>
2021-05-08 22:02:40 +0300

    coment - system

---
+++ app/Http/Livewire/Comments.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Carbon\Carbon;
+use Livewire\Component;
+
+class Comments extends Component
+{
+    public $comments = [
+        [
+            'body'=> 'hello test',
+            'created_at' => '3 min ago',
+            'creator' => 'Mich haio'
+        ]
+    ];
+
+    public function addComment() {
+        array_unshift($this->comments, [
+            'body' => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator' => 'James'
+        ]);
+
+        $this->newComment ="" ;
+    }
+
+    public $newComment;
+
+    public function render()
+    {
+        return view('livewire.comments');
+    }
+}
